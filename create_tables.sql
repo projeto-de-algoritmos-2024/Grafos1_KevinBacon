41d8df644f807af2e3b5be8fe642e895e11ba95a
Hunter104 <000plagueinc@gmail.com>
2024-10-29 20:35:10 -0300

    fixup

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,22 @@
+CREATE TABLE title_basics (
+    tconst TEXT NOT NULL,
+    titleType TEXT NOT NULL,
+    primaryTitle TEXT NOT NULL,
+    originalTitle TEXT NOT NULL,
+    isAdult BOOLEAN NOT NULL,
+    startYear INTEGER NOT NULL,
+    endYear INTEGER,
+    runtimeMinutes INTEGER NOT NULL,
+    CONSTRAINT title_basics_PK PRIMARY KEY (tconst)
+);
+
+CREATE TABLE title_genres (
+    tconst TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    CONSTRAINT title_genres_UK UNIQUE(tconst, genre),
+    CONSTRAINT title_genres_title_basics_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
+);
+
 CREATE TABLE title_akas (
     titleId TEXT NOT NULL,
     ordering INTEGER NOT NULL,
@@ -23,31 +42,11 @@ CREATE TABLE akas_attributes (
     CONSTRAINT akas_attributes_title_akas_FK FOREIGN KEY (titleAkasId) REFERENCES title_akas(titleId)
 );
 
-CREATE TABLE title_basics (
-    tconst TEXT NOT NULL,
-    titleType TEXT NOT NULL,
-    primaryTitle TEXT NOT NULL,
-    originalTitle TEXT NOT NULL,
-    isAdult BOOLEAN NOT NULL,
-    startYear INTEGER NOT NULL,
-    endYear INTEGER,
-    runtimeMinutes INTEGER NOT NULL,
-    CONSTRAINT title_basics_PK PRIMARY KEY (tconst)
-);
-
-CREATE TABLE title_genres (
-    tconst TEXT NOT NULL,
-    genre TEXT NOT NULL,
-    CONSTRAINT title_genres_UQ UNIQUE(tconst, genre),
-    CONSTRAINT title_genres_title_basics_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
-);
-
-
 CREATE TABLE crew_directors (
     tconst TEXT NOT NULL,
     nconst TEXT NOT NULL,
     CONSTRAINT crew_directors_UK UNIQUE(tconst, nconst),
-    CONSTRAINT crew_directors_title_principals_FK FOREIGN KEY (tconst) REFERENCES title_principals(tconst),
+    CONSTRAINT crew_directors_title_basics_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
     CONSTRAINT crew_directors_name_basics_FK FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
 );
 
@@ -55,7 +54,7 @@ CREATE TABLE crew_writers (
     tconst TEXT NOT NULL,
     nconst TEXT NOT NULL,
     CONSTRAINT crew_writers_UK UNIQUE(tconst, nconst),
-    CONSTRAINT crew_writers_title_principals_FK FOREIGN KEY (tconst) REFERENCES title_principals(tconst),
+    CONSTRAINT crew_writers_title_basics_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
     CONSTRAINT crew_writers_name_basics_FK FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
 );
 
@@ -65,8 +64,8 @@ CREATE TABLE title_episode (
     seasonNumber INTEGER NOT NULL,
     episodeNumber INTEGER NOT NULL,
     CONSTRAINT title_episode_PK PRIMARY KEY (tconst),
-    FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
-    FOREIGN KEY (parentTconst) REFERENCES title_basics(tconst)
+    CONSTRAINT title_episode_tconst_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
+    CONSTRAINT title_episode_parent_FK FOREIGN KEY (parentTconst) REFERENCES title_basics(tconst)
 );
 
 CREATE TABLE title_principals (
@@ -77,18 +76,17 @@ CREATE TABLE title_principals (
     job TEXT,
     characters TEXT,
     CONSTRAINT title_principals_UK UNIQUE (tconst, nconst),
-    FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
-    FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
+    CONSTRAINT title_principals_tconst_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
+    CONSTRAINT title_principals_nconst_FK FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
 );
 
 CREATE TABLE title_ratings (
     tconst TEXT PRIMARY KEY,
     averageRating REAL,
     numVotes INTEGER,
-    FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
+    CONSTRAINT title_ratings_tconst_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
 );
 
--- Table: name_basics
 CREATE TABLE name_basics (
     nconst TEXT PRIMARY KEY,
     primaryName TEXT NOT NULL,
@@ -99,12 +97,14 @@ CREATE TABLE name_basics (
 CREATE TABLE name_professions (
     nconst TEXT NOT NULL,
     profession TEXT NOT NULL,
-    FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
+    CONSTRAINT name_professions_UK UNIQUE(nconst, profession),
+    CONSTRAINT name_professions_nconst_FK FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
 );
 
 CREATE TABLE known_for_titles (
     nconst TEXT NOT NULL,
     tconst TEXT NOT NULL,
-    FOREIGN KEY (nconst) REFERENCES name_basics(nconst),
-    FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
+    CONSTRAINT known_for_titles_UK UNIQUE(nconst, tconst),
+    CONSTRAINT known_for_titles_nconst_FK FOREIGN KEY (nconst) REFERENCES name_basics(nconst),
+    CONSTRAINT known_for_titles_tconst_FK FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
 );
